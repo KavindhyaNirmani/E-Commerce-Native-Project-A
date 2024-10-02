b6e3ac2463354e8b4695038923a18eb42225271e
DeviniMunasinghe <98027737+DeviniMunasinghe@users.noreply.github.com>
2024-10-02 08:50:57 +0530

    Implement the image uploading feature in add-admin section

--- Backend/api/routes/authRoutes.js
+++ Backend/api/routes/authRoutes.js
@@ -3,11 +3,32 @@
 //authRoutes define the routes for authentication ,login and admin actions.
 
 const express=require('express');
+const path=require('path');
+const multer=require('multer');
 const authController=require('../controllers/authController');
 const {protect,adminOnly}= require('../middleware/authMiddleware');
 
-const upload = authController.upload;
+
 const router=express.Router();//create a new instance of the Express Router
+
+const absolutePath = path.join('D:\\CODE PARK\\E_Com_Test\\int-24-2-a-ecom-native\\Frontend\\Assets\\UserImage');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, absolutePath);  // Use absolute path to Frontend/Assets
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);// Generate unique filename. Because if different time or different user add files with same name
+        cb(null, uniqueSuffix + '-' + file.originalname);  
+    }
+});
+
+
+const upload =multer({storage:storage});
+
+
+
 
 //User registration
 router.post('/register',authController.register);
@@ -15,8 +36,11 @@ router.post('/register',authController.register);
 //User/Admin login
 router.post('/login',authController.login);
 
+router.use('/Assets/UserImage', express.static(absolutePath));
+
 //Admin creation(admin-only route)
 router.post('/add-admin', protect, adminOnly, upload.single('user_image'), authController.addAdmin);
+
 
 //Get all admins
 router.get('/admins',protect,adminOnly,authController.getAllAdmins);
