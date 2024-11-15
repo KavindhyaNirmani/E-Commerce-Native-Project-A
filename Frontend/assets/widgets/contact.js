9560fe78ab43cd45919d336c46026647d8fab4a9
MaheshikaWijesinghe <maheshika24@stu.codepark.university>
2024-11-15 19:54:56 +0530

    refactor: refactor contact form script with axios

--- Frontend/assets/widgets/contact.js
+++ Frontend/assets/widgets/contact.js
@@ -12,24 +12,20 @@ $(document).ready(function () {
     const contactNumber = $("#contactNumber").val();
     const message = $("#message").val();
 
-    $.ajax({
-      url: " https://ecom-back-t1.netfy.app/api/feedback/send-feedback",
-      method: "POST",
-      contentType: "application/json",
-      data: JSON.stringify({
+    axios
+      .post("https://ecom-back-t1.netfy.app/api/feedback/send-feedback", {
         name: name,
         email: email,
         contactNumber: contactNumber,
         message: message,
-      }),
-      success: function (response) {
+      })
+      .then(function (response) {
         alert("Your message has been sent successfully!");
         $("#contactModal").modal("hide");
         $("#contactForm")[0].reset();
-      },
-      error: function (error) {
+      })
+      .catch(function (error) {
         alert("There was an error sending your message. Please try again.");
-      },
-    });
+      });
   });
 });
